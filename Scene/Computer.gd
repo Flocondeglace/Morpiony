22515cdb28812445414a7bbe1ea4b8b45e0267b2
Floriane Payen <payenf@bde.enseeiht.fr>
2023-10-20 20:46:40 +0200

    code plus jolie

--- Scene/Computer.gd
+++ Scene/Computer.gd
@@ -1,34 +1,46 @@
 extends Node2D
 
+## Contiendra la liste des cases voisines à une case déjà prises par le joueur 
 var hasneighbour = []
+var num_player : int
+var cases_disponibles
+var morpions_disponibles
+## Case que le joueur va jouer
+var choix
+var liste_choix
 
-func play(numPlayer,morp,dispo,morplegal):
-	# stupide
-	#var case = dispo.pick_random().pick_random()#dispo[randi_range(0, dispo.size()-1)]
-	#case.emit_signal("pressed")
-	
-	var choix
+func play_strat_stupide():
+	return play_random(cases_disponibles)
+
+func play_strat_attaque():
 	hasneighbour = []
-	# semi smart attaque
+	liste_choix = []
 	push_warning("Attaque")
-	for minimorplegal in morplegal:
-		choix = play_align_3(numPlayer,minimorplegal.minimorp,true)
-		if choix != null:
-			break
-	if choix == null:
+	liste_choix = play_attack()
+	if liste_choix == []:
 		push_warning("Defense")
-		for minimorplegal in morplegal:
-			choix = play_def(numPlayer,minimorplegal.minimorp)
-			if choix != null:
-				push_warning("break")
-				break
-	if choix==null:
+		liste_choix = play_def()
+	if liste_choix == []:
 		push_warning("Voisin")
-		choix = play_next_to(hasneighbour)
-	if choix == null:
-		push_warning("Random")
-		choix = play_random(dispo)
-	click(numPlayer,choix)
+		liste_choix = hasneighbour
+	if liste_choix == []:
+		push_warning("Aleatoire")
+		liste_choix.append(play_random(cases_disponibles))
+	return play_random_list(liste_choix)
+	
+
+func play(vnum_player,morp,dispo,morplegal):
+	num_player = vnum_player
+	cases_disponibles = dispo
+	morpions_disponibles = morplegal
+	choix = null
+	# stupide
+	# choix = play_strat_stupide()
+
+	# semi smart attaque
+	choix = play_strat_attaque()
+	
+	click(choix)
 	
 
 func play_random(dispo):
@@ -37,18 +49,23 @@ func play_random(dispo):
 func play_random_list(dispo):
 	return dispo.pick_random()
 	
-func click(numPlayer,case):
-	case.set_piece(numPlayer)
+func click(case):
+	case.set_piece(num_player)
 
-# Prends en entrée la liste des cases dispo à côté desquelles il y a une case de rempli gagnante
-func play_next_to(dispo):
-	if dispo != []:
-		play_random_list(dispo)
-	else :
-		return null
-	
+func play_attack():
+	var liste_choix_attack = []
+	for mini in morpions_disponibles:
+		liste_choix_attack += play_attack_mini(num_player,mini.minimorp,true)
+	return liste_choix_attack
 
-func play_align_3(numPlayer,cases,conservation_voisins):
+func play_def():
+	var liste_choix_def = []
+	for mini in morpions_disponibles:
+		liste_choix_def += play_def_mini(mini.minimorp)
+	return liste_choix_def
+
+func play_attack_mini(numPlayer,cases,conservation_voisins):
+	var liste_propositions = []
 	var wincases = cases.filter(func (case): return case.win == numPlayer)
 	var freecases = cases.filter(func (case): return case.win == 0)
 	var wincases_ord_line = []
@@ -61,9 +78,9 @@ func play_align_3(numPlayer,cases,conservation_voisins):
 	var windiag2 = wincases.filter(func (case): return case.pos in [2,4,6])
 	
 	if (freediag1.size() == 1 && windiag1.size()==2):
-		return freediag1[0]
+		liste_propositions.append(freediag1[0])
 	if (freediag2.size() == 1 && windiag2.size()==2):
-		return freediag2[0]
+		liste_propositions.append(freediag2[0])
 	
 	var k : int = 3
 	for i in range (0,3):
@@ -74,18 +91,18 @@ func play_align_3(numPlayer,cases,conservation_voisins):
 	for i in range (0,3):
 		if (wincases_ord_line[i].size() == 2 && freecases_ord_line[i].size() == 1):
 			push_warning("liiigne "+ str(i)+ " vpos : "+str(freecases_ord_line[i][0].pos))
-			return freecases_ord_line[i][0]
+			liste_propositions.append(freecases_ord_line[i][0])
 		if (wincases_ord_column[i].size() == 2 && freecases_ord_column[i].size() == 1):
 			push_warning("colooone "+ str(i)+ " vpos : " + str(freecases_ord_column[i][0].pos))
-			return freecases_ord_column[i][0]
+			liste_propositions.append(freecases_ord_column[i][0])
 		if conservation_voisins :
 			if (wincases_ord_line[i].size()==1 && freecases_ord_line[i].size()>=1):
 				hasneighbour.append_array(freecases_ord_line[i])
 			if (wincases_ord_column[i].size()==1 && freecases_ord_column[i].size()>=1):
 				hasneighbour.append_array(freecases_ord_column[i])
-	push_warning("rien trouvé")
-	return null
+	return liste_propositions
 
 
-func play_def(numPlayer,cases):
-	return play_align_3(numPlayer%2 +1, cases,false)
+func play_def_mini(cases):
+	return play_attack_mini(num_player%2 +1, cases,false)
+	
