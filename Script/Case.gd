835f2844a8cc696e6b4866fa5b2aef565ed9c63e
Floriane Payen <payenf@bde.enseeiht.fr>
2023-10-30 20:46:43 +0100

    rep bug + theme halloween

--- Script/Case.gd
+++ Script/Case.gd
@@ -3,8 +3,8 @@ extends Button
 signal has_play
 
 var case_empty = preload("res://Image/case_empty.png")
-var case_p1 = preload("res://Image/case_circle.png")
-var case_p2 = preload("res://Image/case_cross.png")
+var case_p1 = preload("res://Image/citrouille.png")
+var case_p2 = preload("res://Image/chauve_souris.png")
 
 var case = [case_empty,case_p1,case_p2]
 
@@ -18,12 +18,15 @@ func _ready():
 	set_button_icon(case[0])
 
 func set_piece(numPlayer):
-	set_button_icon(case[numPlayer])
-	win = numPlayer
-	has_play.emit(pos)
-	set_disabled(true)
-	
-	push_warning("pos :" + str(pos))
+	if (!is_disabled()) :
+		set_button_icon(case[numPlayer])
+		win = numPlayer
+		has_play.emit(pos)
+		set_disabled(true)
+		
+		#push_warning("pos :" + str(pos))
+	else :
+		push_warning("questufou" + str(pos))
 
 func _on_pressed():
 	#set_piece(get_parent().get_parent().get_parent().get_parent().player_turn)
